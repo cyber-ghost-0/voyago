42963988c43b9d19ca7bd7a8600c3a1a4666e243
_ANAS_ <anas.sahar.ahmad@gmail.com>
2024-08-27 12:14:34 +0300

    .

--- util/database.js
+++ util/database.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("voyago", "root", "anas", {
   dialect: "mysql",
   host: "localhost",
+  dialectModule: require('mysql2'), // Explicitly specify mysql2
 });
 
 module.exports = sequelize;
